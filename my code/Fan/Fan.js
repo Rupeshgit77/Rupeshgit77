61b70c49ba8dcf75ad0a48155c5322f9876e7a33
Sowmya Ari <sowmya.ari@everest.engineering>
2024-02-11 05:23:45 +1200

    my first commit

---
+++ my code/Fan/Fan.js
@@ -0,0 +1,11 @@
+let a=document.getElementById("img");
+function myfunon()
+{a.style.animationDuration=3+"s";}
+function myfunoff()
+{a.style.animationDuration=0+"s";}
+function myfun2()
+{a.style.animationDuration=1+"s";}
+function myfun3()
+{a.style.animationDuration=0.5+"s";}
+function myfun4()
+{a.style.animationDuration=0.1+"s";}
---
+++ my code/index.js
@@ -0,0 +1,6 @@
+//console.log(`hello Rupesh`);
+//console.log(`1+2`);
+// window.alert(`hello node`);
+
+
+ Document.getElementById("js").textContent="hello rupesh";
